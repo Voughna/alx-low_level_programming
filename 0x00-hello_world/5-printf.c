2ec6643f106a22cdfb96c067b9463b66345ff52d
Voughna <matongomil@gmail.com>
2023-02-20 11:51:31 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * main- A program that prints using the printf function
- * * Return: Always 0 (Success)
+ * Return: 0(Success)
  */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
